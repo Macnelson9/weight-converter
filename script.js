4a6c2f5a652b9b41b5d87d2fbbfe1d9dc5907f3c
Macnelson9 <michaelofatu@gmail.com>
2025-01-23 00:10:49 +0100

    Add Functionality and Logic to App For this commit, the app logic is implemented.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const userInput = document.getElementById('input');
+const resultEl = document.getElementById('result');
+const errorEl = document.getElementById('error');
+
+userInput.addEventListener('click', function () {
+  const kilograms = Number(userInput.value) * 0.4536;
+  console.log(typeof kilograms);
+  console.log(kilograms);
+
+  return kilograms;
+});
