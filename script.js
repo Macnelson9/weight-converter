0342948a58f7433321cddb47cf970380912b7d44
Macnelson9 <michaelofatu@gmail.com>
2025-01-24 15:40:33 +0100

    Add and Implement the app functionality and logic

--- script.js
+++ script.js
@@ -3,11 +3,35 @@
 const userInput = document.getElementById('input');
 const resultEl = document.getElementById('result');
 const errorEl = document.getElementById('error');
+const convertBtn = document.getElementById('convertBtn');
 
-userInput.addEventListener('click', function () {
+/// Logic to convert the user weight
+convertBtn.addEventListener('click', function () {
   const kilograms = Number(userInput.value) * 0.4536;
-  console.log(typeof kilograms);
-  console.log(kilograms);
+
+  if (userInput.value < 1) {
+    userInput.style.borderColor = 'red';
+    errorEl.textContent = 'Please enter a valid number!';
+  } else {
+    userInput.style.borderColor = 'green';
+    resultEl.textContent = kilograms;
+    errorEl.textContent = '';
+  }
+
+  setTimeout(
+    () => ((userInput.value = ''), (resultEl.textContent = '')),
+    10000
+  );
 
   return kilograms;
+});
+
+userInput.addEventListener('focus', function () {
+  if (userInput.value < 1 || userInput.value === '') {
+    userInput.style.borderColor = 'red';
+    errorEl.textContent = 'Please enter a valid number!';
+  } else {
+    userInput.style.borderColor = 'green';
+    // errorEl.textContent = '';
+  }
 });
